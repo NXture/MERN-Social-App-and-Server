324f130ec68db7841ca774ab34f4e3fa3264ec58
NXture <varunvaru27101999@gmail.com>
2021-09-26 19:16:44 +0530

    fixed package error

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const { ApolloServer } = require("apollo-server");
-const mongoose = require("mongoose");
+const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
 
-const typeDefs = require("./graphql/typeDefs");
-const resolvers = require("./graphql/resolvers");
-const { MONGODB } = require("./config.js");
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
+
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req }),
+  context: ({ req }) => ({ req, pubsub })
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log("MongoDB Connected");
+    console.log('MongoDB Connected');
     return server.listen({ port: 5000 });
   })
   .then((res) => {
